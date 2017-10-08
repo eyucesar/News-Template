5db87c2666212324af9cc8b02178a3836ba93c12
Ebru Yucesar <ebru.yucesar@gmail.com>
2017-10-08 00:10:40 -0500

    sidebar js and media queries added

--- js/app.js
+++ js/app.js
@@ -18,9 +18,17 @@ $(document).ready(function() {
 				$("ul li").eq(i).attr("backGr", articles[i].urlToImage);
 				$("ul li").eq(i).attr("noBackGr", "background: none");
 				$("ul li").eq(i).attr("data-state", "noBackGr");
-				$("ul li").eq(i).on("mouseover", changeBackGr);
+				//calling the change background function
+				$("ul li").eq(i).hover(changeBackGr);
+
 			}
-		
+			//first article populates the content div on page load
+			$("#content").html("<article></article>");
+			$("article").append("<h1>" + articles[0].title + "</h1>");
+			$("article").append("<img src='" + articles[0].urlToImage + "' alt='article-image'>");
+			$("article").append("<i class='fa fa-rss-square' aria-hidden='true'></i><i class='fa fa-twitter-square' aria-hidden='true'></i><i class='fa fa-facebook-square' aria-hidden='true'></i><i class='fa fa-envelope' aria-hidden='true'></i>");
+			$("article").append("<h4>By " + articles[0].author + " - " + moment(articles[0].publishedAt).format('ll') + "</h4>");
+			$("article").append(articles[0].long_description);
 		});
 	}
 
